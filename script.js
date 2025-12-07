7e41b30be64d0ebd7c74f6090f50e56dfade678c
Shreyasi53 <shreyasidutta53@gmail.com>
2025-12-07 13:22:26 +0530

    Implement password generation and copy functionality; enhance responsive design for better UI

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
 const passwordBox = document.getElementById("password");
+const length = 8;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%&*_()=<>[]/~";
+
+const allChars = upperCase + lowerCase + number + symbol;
+
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];//it will generate any random value 0-25 and according to that number uppercase character select
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while(length > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+}
+
+function copyPassword(){
+    passwordBox.select();
+    document.execCommand("copy");
+    alert("Password Copied Successfully!");
+}
+
+document.getElementById("button").addEventListener("click", createPassword)
+document.getElementById("copy").addEventListener("click", copyPassword)
